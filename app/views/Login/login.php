17aa309a914fb3fb12e856fc114b61b8d83d5e54
Ezequiel Lagos <ezequiel.lagos@alumnos.upla.cl>
2018-08-08 16:09:49 -0400

    UPDATE: se modifica el diseño del login

--- app/views/Login/login.php
+++ app/views/Login/login.php
@@ -1,65 +1,107 @@
-<style>
-	html,
-	body {
-	  height: 100%;
-	}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="X-UA-Compatible" content="id=edge">
 
-	body {
-	  display: -ms-flexbox;
-	  display: -webkit-box;
-	  display: flex;
-	  -ms-flex-align: center;
-	  -ms-flex-pack: center;
-	  -webkit-box-align: center;
-	  align-items: center;
-	  -webkit-box-pack: center;
-	  justify-content: center;
-	  padding-top: 40px;
-	  padding-bottom: 40px;
-	  background-color: #f5f5f5;
-	}
+	<!-- Tipografía Universidad de Playa Ancha -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900">
 
-	.form-signin {
-	  width: 100%;
-	  max-width: 330px;
-	  padding: 15px;
-	  margin: 0 auto;
-	}
-	.form-signin .checkbox {
-	  font-weight: 400;
-	}
-	.form-signin .form-control {
-	  position: relative;
-	  box-sizing: border-box;
-	  height: auto;
-	  padding: 10px;
-	  font-size: 16px;
-	}
-	.form-signin .form-control:focus {
-	  z-index: 2;
-	}
-	.form-signin input[type="email"] {
-	  margin-bottom: -1px;
-	  border-bottom-right-radius: 0;
-	  border-bottom-left-radius: 0;
-	}
-	.form-signin input[type="password"] {
-	  margin-bottom: 10px;
-	  border-top-left-radius: 0;
-	  border-top-right-radius: 0;
-	}
-</style>
+	<!-- Material for Bootstrap CSS -->
+	<link rel="stylesheet" type="text/css" href="<?= ROUTE_URL ?>css/bootstrap-material-design.min.css">
+	<!-- Custom CSS -->
+	<link rel="stylesheet" type="text/css" href="<?= ROUTE_URL ?>css/main.css">
+
+	<link rel="shortcut icon" href="http://www.upla.cl/portada/wp-content/uploads/2015/05/favicon.ico" />
+
+	<title><?= NAME_SITE ?></title>
+
+	<style type="text/css">
+		
+	</style>
+
+</head>
+<body>
 
 
-<div class="text-center">
-	<form class="form-signin" method="POST" action="<?= ROUTE_URL ?>Login/">
-		<h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-		<label for="inputEmail" class="sr-only">Email address</label>
-		<input type="text" name="email" id="inputEmail" class="form-control" placeholder="Email address" value="<?= $email ?>" autofocus>
-		<label for="inputPassword" class="sr-only">Password</label>
-		<input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password">
-		<p><?= $errorMessage ?></p>
-		<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-		<p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-	</form>
+<div class="container h-100">
+	<div class="row h-100">
+		<div class="col-md-12 align-self-center">
+			<div class="row">
+				<div class="col-md-6 mx-auto">
+
+					<!-- form card login -->
+					<div class="card rounded-0">
+						<div class="card-header">
+							<h3 class="mb-0">Login</h3>
+						</div>
+						<div class="card-body">
+							<form class="form" role="form" autocomplete="off" id="formLogin" novalidate="" method="POST" action="<?= ROUTE_URL ?>Login/">
+								<div class="form-group">
+									<label for="email">Correo</label>
+									<input type="text" class="form-control form-control-lg rounded-0" name="email" id="email" value="<?= $email ?>" required="">
+									<div class="invalid-feedback">No olvide colocar su correo</div>
+								</div>
+								<div class="form-group">
+									<label for="">Contraseña</label>
+									<input type="password" name="password" class="form-control form-control-lg rounded-0" id="pwd1" required="" autocomplete="new-password">
+									<div class="invalid-feedback">No olvide colocar su contraseña</div>
+								</div>
+								<div class="form-group">
+									<div class="alert alert-danger" id="mensajeAlerta" role="alert"><?= $errorMessage ?></div>
+								</div>
+								<button type="submit" class="btn btn-success btn-lg float-right" id="btnLogin">Login</button>
+							</form>
+						</div>
+						<!--/card-block-->
+					</div>
+					<!-- /form card login -->
+
+				</div>
+			</div>
+			<!--/row-->
+		</div>
+		<!--/col-->
+	</div>
+	<!--/row-->
 </div>
+<!--/container-->
+
+
+<?php 
+/*
+	Elementos que deben estar si o si al realizar el login
+		- input:text|email;  name = email; value = <?= $email ?>
+		- input:password;  name = password
+		- elemento que contendra los errores del login. Debe contener este valor = <?= $errorMessage ?>
+*/
+ ?>
+
+
+<script src="<?= ROUTE_URL ?>js/jquery-3.3.1.min.js"></script>
+<script src="<?= ROUTE_URL ?>js/popper-umd-1.12.6.js"></script>
+<script src="<?= ROUTE_URL ?>js/bootstrap-material-design.min.js"></script>
+<script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
+
+<script src="<?= ROUTE_URL ?>/js/main.js"></script>
+<script type="text/javascript">
+	$("#btnLogin").click(function(event) {
+
+	//Fetch form to apply custom Bootstrap validation
+	var form = $("#formLogin")
+
+	if (form[0].checkValidity() === false) {
+	  event.preventDefault()
+	  event.stopPropagation()
+	}
+	
+	form.addClass('was-validated');
+  });
+
+	if ($('#mensajeAlerta').is(':empty')){
+		$('#mensajeAlerta').hide();
+	}
+</script>
+</body>
+</html>
