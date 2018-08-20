51b1b8b5027a66eb80a726777b51cbc31df500a6
Ezequiel Lagos <ezequiel.lagos@alumnos.upla.cl>
2018-08-20 16:28:59 -0300

    UPDATE: se cambia de nombre una clase para acerla más reconocible

--- app/controllers/AcademicosController.php
+++ app/controllers/AcademicosController.php
@@ -8,16 +8,35 @@ class AcademicosController extends Controller
 	
 	public function __construct()
 	{
-
+		$this->academicoModel = $this->model('Academico');
 	}
 
 	public function index()
 	{
+		$resultado = $this->academicoModel->getAcademicos();
+
 		$data = [
 			'active' => 'academicos',
 			'title' => 'Académicos',
+			'academicos' => $resultado,
 		];
 
 		$this->view('Inicio/academicos', $data);
+	}
+
+	public function academico($id = '')
+	{
+		if (empty($id)) {
+			return $this->index();
+		}
+		
+		$resultado = $this->academicoModel->getAcademicoById($id);
+
+		$data = [
+			'active' => 'academicos',
+			'title' => $resultado->nombre,
+			'academico' => $resultado,
+		];
+		$this->view('Inicio/academico', $data);
 	}
 }
---
+++ app/models/AcademicoModel.php
@@ -0,0 +1,63 @@
+<?php defined('BASEPATH') or exit('No se permite el acceso directo');
+
+/**
+ * 
+ */
+class AcademicoModel extends DBConnect
+{
+	private $db;
+	
+	function __construct()
+	{
+		$this->db = new DBConnect();
+	}
+
+	public function getAcademicoById($id)
+	{
+		$this->db->query("SELECT * FROM profesor WHERE id = :id");
+		$this->db->bind(':id', $id);
+		return $this->db->record();
+	}
+
+	public function getAcademicos()
+	{
+		$this->db->query("SELECT * FROM profesor");
+		return $this->db->records();
+	}
+
+	public function addCategoria($data)
+	{
+		$this->db->query('INSERT INTO categoria (nombre, descripcion) VALUES (:nombre, :descripcion)');
+		$this->db->bind(':nombre', $data['nombre']);
+		$this->db->bind(':descripcion', $data['descripcion']);
+
+		if ($this->db->execute())
+			return true;
+		else
+			return false;
+	}
+
+	public function updateCategoria($data)
+	{
+		$this->db->query('UPDATE categoria SET nombre = :nombre, descripcion = :descripcion WHERE id = :id');
+		$this->db->bind(':id', $data['id']);
+		$this->db->bind(':nombre', $data['nombre']);
+		$this->db->bind(':descripcion', $data['descripcion']);
+
+		if ($this->db->execute())
+			return true;
+		else
+			return false;
+	}
+
+	public function deleteCategoria($id)
+	{
+		$this->db->query('DELETE FROM categoria WHERE id = :id');
+		$this->db->bind(':id', $id);
+
+		if ($this->db->execute())
+			return true;
+		else
+			return false;
+	}
+}
